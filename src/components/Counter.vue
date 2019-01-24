0b8508cbeb5a15a70c3d76e3d6864e7167e825d1
sciuridae0603 <sciuridae0603@gmail.com>
2019-01-24 12:52:30 +0800

    fix binding

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -14,7 +14,7 @@
 import Input from "./Counter/Input.vue";
 import List from "./Counter/List.vue";
 
-let Base64 = require('js-base64').Base64;
+let Base64 = require("js-base64").Base64;
 
 export default {
   name: "Counter",
@@ -25,6 +25,22 @@ export default {
   props: {
     items: Array
   },
+  created() {
+    if (location.hash.length != 0) {
+      let data = [];
+      let tmp = JSON.parse(Base64.decode(location.hash));
+      let i = 0;
+      tmp.forEach(el => {
+        data.push({
+          id: i,
+          name: el,
+          number: 0
+        });
+        i = i + 1;
+      });
+      this.items = data;
+    }
+  },
   methods: {
     addItem(value) {
       if (this.items == undefined) {
@@ -32,15 +48,15 @@ export default {
       }
       var id =
         this.items.length == 0 ? 0 : this.items[this.items.length - 1].id + 1;
-      if (value == ""){
-        return
+      if (value == "") {
+        return;
       }
       this.items.push({ name: value, number: 0, id: id });
-      
+
       let tmp = [];
-      this.items.forEach((el) => {
-        tmp.push(el.name)
-      })
+      this.items.forEach(el => {
+        tmp.push(el.name);
+      });
       location.hash = Base64.encode(JSON.stringify(tmp));
     },
     deleteItem(id) {
@@ -61,8 +77,8 @@ export default {
     lessItem(id) {
       for (var i in this.items) {
         if (this.items[i].id == id) {
-          if(this.items[i].number == 0){
-            return
+          if (this.items[i].number == 0) {
+            return;
           }
           this.items[i].number = this.items[i].number - 1;
           break;
--- src/main.js
+++ src/main.js
@@ -3,26 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-let Base64 = require('js-base64').Base64;
-
-let data = [];
-if(location.hash.length != 0){  
-  let tmp = JSON.parse(Base64.decode(location.hash));
-  let i = 0;
-  tmp.forEach((el) => {
-    data.push({
-      id : i,
-      name : el,
-      number: 0
-    })
-    i=i+1;
-  })
-}
-
 new Vue({
   render: h => h(App, {
     props: {
-      items: data
     }
   })
 }).$mount('#app')
